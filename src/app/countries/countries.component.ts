5abefff4f3eca823928c066bbcd0b32a3dfa8cf8
Hugo-Fonseca739 <142123500+Hugo-Fonseca@users.noreply.github.com>
2025-03-26 13:36:21 -0500

    Logs de consola

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonContent, IonRow, IonHeader, IonCard, IonCardHeader, IonToolbar, IonGrid, IonTitle, IonCol, IonCardTitle, IonCardSubtitle, IonSearchbar } from "@ionic/angular/standalone";
+import { IonContent, IonRow, IonHeader, IonCard, IonCardHeader, IonToolbar, IonGrid, IonTitle, IonCol, IonCardTitle, IonCardSubtitle, IonSearchbar, IonButton } from "@ionic/angular/standalone";
 import { FilterCountriesPipe, SearchCountriesDirective } from 'src/core/directives/search-countries.directive';
 import { CountriesService } from 'src/core/services/countries.service';
+
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.scss'],
-  imports: [IonCardSubtitle, IonGrid, IonCard, IonContent, IonRow, IonHeader, IonCardHeader, IonToolbar, IonTitle, IonCol, IonCardTitle, CommonModule, IonSearchbar, FilterCountriesPipe, SearchCountriesDirective]
+  imports: [IonCardSubtitle, IonGrid, IonCard, IonContent, IonRow, IonHeader, IonCardHeader, IonToolbar, IonTitle, IonCol, IonCardTitle, CommonModule, IonSearchbar, FilterCountriesPipe, SearchCountriesDirective, IonButton]
 })
-export class CountriesComponent  implements OnInit {
+export class CountriesComponent implements OnInit {
   paises: any[] = [];
   paisesFiltrados: any[] = [];
   terminoBusqueda: string = '';
+  logs: string[] = []; // Almacena los países visitados en memoria
 
   constructor(
     private countriesService: CountriesService,
@@ -37,8 +39,18 @@ export class CountriesComponent  implements OnInit {
   }
 
   irADetallePais(pais: any) {
+    // Guardar en el log la selección del país
+    this.logs.push(`Visitaste: ${pais.name.common}`);
+    console.log(this.logs); // Mostrar en la consola
+
     // Navegar usando el nombre del país como parámetro
     this.router.navigate(['/paises/detalle', pais.name.common]);
+  }
+
+  // Método para borrar los logs
+  limpiarLogs() {
+    this.logs = [];
+    console.log("Logs borrados");
   }
 
   // Método para manejar la búsqueda
